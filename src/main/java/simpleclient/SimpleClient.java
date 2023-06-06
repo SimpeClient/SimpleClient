42e0297a3a4c05c49f51d44ad16c6d9106c071a8
Noriskky <Lefoxx86@gmail.com>
2023-06-06 13:36:30 +0200

    +Main Class

---
+++ src/main/java/simpleclient/SimpleClient.java
@@ -0,0 +1,11 @@
+package simpleclient;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleClient implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
